e585ac3380c034ec3cf7746e5377b0427afba9e3
Your Name <you@example.com>
2022-10-28 18:06:35 +0000

    lstm_loadweight

--- abc-PegInsertionSide-cont.sh
+++ abc-PegInsertionSide-cont.sh
@@ -16,7 +16,7 @@ python3.8 maniskill2_learn/apis/run_rl.py configs/mfrl/$METHOD/maniskill2_pn.py 
 "eval_cfg.num=100" "eval_cfg.save_traj=False" "eval_cfg.save_video=True" \
 "eval_cfg.num_procs=5" \
 "train_cfg.n_steps=200" \
-"train_cfg.total_steps=25000000" "train_cfg.n_checkpoint=500000" \
+"train_cfg.total_steps=25000000" "train_cfg.n_checkpoint=500000" "train_cfg.n_eval=100000" \
 "replay_cfg.sampling_cfg.type=TStepTransition" \
 "replay_cfg.sampling_cfg.with_replacement=False" \
 "replay_cfg.sampling_cfg.horizon=$STEP" \
--- abc-PegInsertionSide-nstep5-w1-load.sh
+++ abc-PegInsertionSide-nstep5-w1-load.sh
@@ -1,13 +1,14 @@
 METHOD="abc"
 ENV="PegInsertionSide"
-GPU="7"
+GPU="6"
 STEP="5"
-WEIGHT="10"
+WEIGHT="1"
 
-YOUR_LOGGING_DIRECTORY="logs/$METHOD-$ENV-step$STEP-w$WEIGHT.log"
+YOUR_LOGGING_DIRECTORY="logs/$METHOD-$ENV-step$STEP-w$WEIGHT-load.log"
 
 python3.8 maniskill2_learn/apis/run_rl.py configs/mfrl/$METHOD/maniskill2_pn.py \
 --work-dir $YOUR_LOGGING_DIRECTORY --gpu-ids $GPU --sim-gpu-ids $GPU \
+--resume-from ./logs/dapg-PegInsertionSide-log/models/model_10000000.ckpt \
 --cfg-options "env_cfg.env_name=$ENV-v0" "env_cfg.obs_mode=pointcloud" \
 "env_cfg.n_points=1200" "env_cfg.obs_frame=ee" "env_cfg.control_mode=pd_ee_delta_pose" \
 "env_cfg.reward_mode=dense" "rollout_cfg.num_procs=16" \
@@ -16,7 +17,7 @@ python3.8 maniskill2_learn/apis/run_rl.py configs/mfrl/$METHOD/maniskill2_pn.py 
 "eval_cfg.num=100" "eval_cfg.save_traj=False" "eval_cfg.save_video=True" \
 "eval_cfg.num_procs=5" \
 "train_cfg.n_steps=200" \
-"train_cfg.total_steps=25000000" "train_cfg.n_checkpoint=500000" \
+"train_cfg.total_steps=25000000" "train_cfg.n_checkpoint=500000" "train_cfg.n_eval=500000" \
 "replay_cfg.sampling_cfg.type=TStepTransition" \
 "replay_cfg.sampling_cfg.with_replacement=False" \
 "replay_cfg.sampling_cfg.horizon=$STEP" \

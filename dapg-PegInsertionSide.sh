3904cfd8a5482e0eda71a52e933b474a47878725
Your Name <you@example.com>
2022-11-10 10:54:05 +0800

    main

--- dapg-PegInsertionSide.sh
+++ dapg-PegInsertionSide.sh
@@ -1,11 +1,11 @@
 METHOD="dapg"
 ENV="PegInsertionSide"
-GPU="2"
+GPU="0"
 
 YOUR_LOGGING_DIRECTORY="logs/$METHOD-$ENV-log"
 
 python3.8 maniskill2_learn/apis/run_rl.py configs/mfrl/$METHOD/maniskill2_pn.py \
---work-dir $YOUR_LOGGING_DIRECTORY --gpu-ids $GPU \
+--work-dir $YOUR_LOGGING_DIRECTORY --gpu-ids $GPU --sim-gpu-ids $GPU \
 --cfg-options "env_cfg.env_name=$ENV-v0" "env_cfg.obs_mode=pointcloud" \
 "env_cfg.n_points=1200" "env_cfg.obs_frame=ee" "env_cfg.control_mode=pd_ee_delta_pose" \
 "env_cfg.reward_mode=dense" "rollout_cfg.num_procs=5" \
